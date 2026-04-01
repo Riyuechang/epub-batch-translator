2aa7cb527533c97cb01518d35daf912827f83cb7
Riyuechang <tsps99187@gmail.com>
2026-04-01 09:38:36 +0800

    新增 icon 和 image.py

---
+++ ui/image.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+from utils.common_tools import get_path_dict
+from utils.dataclass_tools import dataclass_pre_init, set_dataclass_value
+
+
+ICON_PATH = Path("./ui/icon")
+
+
+@dataclass_pre_init
+class Icon:
+    copy: str
+    save: str
+    delete: str
+    refresh: str
+
+    def __post_init__(self):
+        self.load_image()
+
+    def load_image(self):
+        icon_files = get_path_dict(ICON_PATH)
+
+        set_dataclass_value(self, icon_files)
+
+
+icon = Icon()
