da8ba12176ca2f9cb5bd47081f3d6b41d608ba58
Riyuechang <tsps99187@gmail.com>
2026-04-07 23:41:48 +0800

    調整文字大小

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = QApplication(sys.argv)
 
 font_id = QFontDatabase.addApplicationFont("./ui/font/MapleMonoNormalNL-CN-Regular.ttf")
 font_family = QFontDatabase.applicationFontFamilies(font_id)[0]
-font = QFont(font_family, 11)
+font = QFont(font_family, 12)
 
 app.setFont(font)
 
