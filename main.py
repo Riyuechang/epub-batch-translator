af7eaf823e29be806bcfe85fd41528dc93be59a0
Riyuechang <tsps99187@gmail.com>
2026-04-07 23:35:37 +0800

    使用等寬字體

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import sys
 
+from PyQt6.QtGui import QFont, QFontDatabase
 from PyQt6.QtWidgets import QApplication
 
 from ui.windows import MainWindow
 
 
 app = QApplication(sys.argv)
+
+font_id = QFontDatabase.addApplicationFont("./ui/font/MapleMonoNormalNL-CN-Regular.ttf")
+font_family = QFontDatabase.applicationFontFamilies(font_id)[0]
+font = QFont(font_family, 11)
+
+app.setFont(font)
 
 window = MainWindow()
 window.show()
